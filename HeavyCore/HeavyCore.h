6b720351a83df68d178777117d8e9b8049fec598
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-04-27 17:20:20 -0400

    initial commit

---
+++ HeavyCore/HeavyCore.h
@@ -0,0 +1,19 @@
+//
+//  HeavyCore.h
+//  HeavyCore
+//
+//  Created by Dylan Wreggelsworth on 4/27/16.
+//  Copyright © 2016 Heavy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeavyCore.
+FOUNDATION_EXPORT double HeavyCoreVersionNumber;
+
+//! Project version string for HeavyCore.
+FOUNDATION_EXPORT const unsigned char HeavyCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeavyCore/PublicHeader.h>
+
+
