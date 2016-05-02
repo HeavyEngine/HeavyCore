54c40b918aa1cc4197e5ec7659f327e091fd8e1f
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-05-02 18:11:13 -0400

    - Basic project setup - Added playground - Basic entity setup - Point struct created

---
+++ Common/Entity.swift
@@ -0,0 +1,12 @@
+//
+//  Entity.swift
+//  HeavyCore
+//
+//  Created by Dylan Wreggelsworth on 5/2/16.
+//  Copyright © 2016 Heavy. All rights reserved.
+//
+
+
+public protocol Entity {
+  var position: Point {get set}
+}
