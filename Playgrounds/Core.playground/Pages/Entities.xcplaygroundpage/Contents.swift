1d0c45b0e49cf93ec5f6da94b78def6e96d057a5
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-05-13 13:10:58 -0400

    do the example the right way

--- Playgrounds/Core.playground/Pages/Entities.xcplaygroundpage/Contents.swift
+++ Playgrounds/Core.playground/Pages/Entities.xcplaygroundpage/Contents.swift
@@ -17,11 +17,11 @@ class ColorBehavior: Behavior {
 
   var id: Int?
   var color: Colors = .Red
-  var tick: Bool = false
+  var flip = false
   func update(delta: Double) {
     //alternate colors
-    tick = tick ? false : true
-    color = tick ? .Blue : .Red
+    flip = flip ? false : true
+    color = flip ? .Blue : .Red
   }
 }
 
@@ -35,10 +35,13 @@ myEntity.add(MotionBehavior())
 // Simulate a game loop...
 for time in 0.stride(to: 1, by: 1.0/15) {
   myEntity.update(time)
-  let pos = myEntity.find(MotionBehavior).first as? MotionBehavior
-  let color = myEntity.find(ColorBehavior).first as? ColorBehavior
-  print(pos?.position)
-  print(color?.color)
+  guard let pos   = myEntity.find(MotionBehavior).first as? MotionBehavior,
+            color = myEntity.find(ColorBehavior).first as? ColorBehavior
+  else {
+      continue
+  }
+  pos.position
+  color.color // Should alternate
 }
 
 //: [Next](@next)
