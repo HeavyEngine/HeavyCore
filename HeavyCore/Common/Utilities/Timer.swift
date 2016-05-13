509a68c1bbef9edb56fb921e1a6a9ca14f4501c1
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-05-12 22:58:49 -0400

    cleanup declarations, add update and FPS measuring/averaging/deltas

--- HeavyCore/Common/Utilities/Timer.swift
+++ HeavyCore/Common/Utilities/Timer.swift
@@ -7,7 +7,6 @@
 //
 
 public struct Timer {
-  private var previousTime: Double = 0
 
   private var period: Double {
     var info = mach_timebase_info_data_t(numer: 0, denom: 0)
@@ -19,17 +18,43 @@ public struct Timer {
     return Double(mach_absolute_time()) * period
   }
 
-  public var currentTime: Double = 0
-  public var averageDelta: Double = 0
-  public var deltaTime: Double = 0
+  public var previousTime = 0.0,
+             currentTime  = 0.0,
+             averageDelta = 0.0,
+             deltaTime    = 0.0,
+             fps          = 0
 
-  public init() {} // Dumb swift/playgrounds compiler!
+
+  private var totalFrames         = 0,
+              priorFPSUpdateTime  = 0.0,
+              fpsUpdateFrequency  = 1.0,
+              timeSinceLastUpdate = 0.0
+
+  public init() {
+    self.priorFPSUpdateTime = time
+    self.currentTime = time
+  }
 }
 
 public extension Timer {
+
+  /// Update the timer to help measure fps, latency etc.
+  /// - TODO: Turn this into something useful for actions, update ticks for the game loop...
+  /// - TODO: Survey it and make sure we're robust enough, maybe improve to something like http://gafferongames.com/game-physics/fix-your-timestep/
+
   mutating func update() {
+    totalFrames += 1
     previousTime = currentTime
+    currentTime = self.time
     deltaTime = currentTime - previousTime
+    timeSinceLastUpdate = currentTime - priorFPSUpdateTime
+
+    if timeSinceLastUpdate > fpsUpdateFrequency {
+      fps = Int((Double(totalFrames) / timeSinceLastUpdate) + 0.5)
+      averageDelta = timeSinceLastUpdate / Double(totalFrames)
+      priorFPSUpdateTime = currentTime
+      totalFrames = 0
+    }
   }
 
 }
