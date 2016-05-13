0de2641da4f32f67972c5dac1de5796b2336729f
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-05-12 22:26:56 -0400

    - Epsilon -> Delta - Better wording for round, aboutEqual, - Added average to an array extension

--- HeavyCore/Common/Utilities/Math.swift
+++ HeavyCore/Common/Utilities/Math.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 public struct Math {
-  /// Determine if the two numbers are within epsilon distance from each other.
+  /// Determine if the two numbers are within a distance from each other.
   /// Based on http://floating-point-gui.de/errors/comparison/
-  public static func aboutEqual(a: Double, b: Double, epsilon: Double = DBL_EPSILON) -> Bool {
+  public static func aboutEqual(a: Double, _ b: Double, delta: Double = DBL_EPSILON) -> Bool {
     let absA = abs(a)
     let absB = abs(b)
     let diff = abs(a - b)
@@ -19,9 +19,9 @@ public struct Math {
     if a == b {
       return true
     } else if ( a == 0 || b == 0 || diff < DBL_MIN) {
-      return diff < (epsilon * DBL_MIN)
+      return diff < (delta * DBL_MIN)
     } else {
-      return diff / min((absA + absB), DBL_MAX) < epsilon
+      return diff / min((absA + absB), DBL_MAX) < delta
     }
   }
 
@@ -37,9 +37,9 @@ public struct Math {
   /// - parameter value: The initial value.
   /// - parameter place: The decimal place you wish to round to.
   /// - parameter base: The base to round in, 10 is the default for decimals.
-  public static func round(value: Double, toPlace place: Int = 0, base: Int = 10) -> Double {
+  public static func round(value: Double, toPlace place: Int = 0, by base: Int = 10) -> Double {
     let p = pow(Double(base), -1.0 * Double(place))
-    return round(value * p) / p
+    return Foundation.round(value * p) / p
   }
 
   public static func clamp(value: Double, min: Double, max: Double) -> Double {
@@ -59,11 +59,25 @@ public struct Math {
 
 public extension Double {
 
-  func isAboutEqual(to b: Double, within epsilon: Double = DBL_EPSILON) -> Bool {
-    return Math.aboutEqual(self, b: b, epsilon: epsilon)
+  func isAboutEqual(to b: Double, within delta: Double = DBL_EPSILON) -> Bool {
+    return Math.aboutEqual(self, b, delta: delta)
   }
 
   func round(toPlace place: Int, base: Int = 10) -> Double {
-    return Math.round(self, toPlace: place, base: base)
+    return Math.round(self, toPlace: place, by: base)
+  }
+
+  func clamp(min min: Double, max: Double) -> Double {
+    return Math.clamp(self, min: min, max: max)
+  }
+
+  func lerp(to end: Double, weight: Double) -> Double {
+    return Math.lerp(self, end: end, weight: weight)
+  }
+}
+
+public extension _ArrayType where Generator.Element == Double {
+  func average() -> Double {
+    return Math.average(self as! [Double])
   }
 }
--- Playgrounds/Core.playground/Pages/Math.xcplaygroundpage/Contents.swift
+++ Playgrounds/Core.playground/Pages/Math.xcplaygroundpage/Contents.swift
@@ -7,29 +7,29 @@ let b = 0.11025123
 
 a.isAboutEqual(to: b)
 
-Math.aboutEqual(a, b: b, epsilon: 0.0002)
+Math.aboutEqual(a, b, delta: 0.0002)
 
 let values = [1.0,2.0]
 Math.average(values)
 
-Math.roundTo(M_PI,place: -1)
-Math.roundTo(M_PI,place: -2)
-Math.roundTo(M_PI,place: -3)
-Math.roundTo(M_PI,place: -4)
-Math.roundTo(M_PI,place: -5)
-Math.roundTo(M_PI,place: -6)
-Math.roundTo(M_PI,place: -7)
+Math.round(M_PI, toPlace: -1)
+Math.round(M_PI, toPlace: -2)
+Math.round(M_PI, toPlace: -3)
+Math.round(M_PI, toPlace: -4)
+Math.round(M_PI, toPlace: -5)
+Math.round(M_PI, toPlace: -6)
+Math.round(M_PI, toPlace: -7)
 
 M_PI.round(toPlace: -3)
 
 Math.clamp(-0.5, min: 0, max: 1)
-Math.clamp(0.5, min: 0, max: 1)
-Math.clamp(1.5, min: 0, max: 1)
+
+1.5.clamp(min: 0, max: 1)
 
 var percent = 0.00
-for value in 0.stride(to: 100, by: 1) {
-  percent = Double(value)/100
-  Math.lerp(0, end: 100, weight: percent)
+for value in 0.stride(to: 20, by: 1) {
+  percent = Double(value)/20
+  Math.lerp(0, end: 20, weight: percent)
 }
 
 //: [Next](@next)
