68e6c9a7acba3dc4e9b95cd1027f26f2787da056
drhurdle <drhurdle@Daniels-MacBook-Pro.local>
2016-05-15 03:03:19 -0400

    Added math functions

--- HeavyCore/Common/Utilities/Math.swift
+++ HeavyCore/Common/Utilities/Math.swift
@@ -52,6 +52,94 @@ public struct Math {
       return value
     }
   }
+  
+  //Determine if two numbers are within a distance of each other
+  //Not sure if i'm missing something but may be cleaner than current version : aboutEqual
+  public static func closeToEqual(a: Double, b: Double, delta: Double? = 0) -> Bool {
+    
+    return abs(a-b) <= delta!;
+    
+  }
+  
+  //Determine if a number is no more than a specific distance above another
+  public static func closeUnderLimit(a: Double, b: Double, delta: Double? = 0) -> Bool {
+    
+    return a <= (b + delta!);
+    
+  }
+  
+  //Determine if a number is no less than a specific distance below another
+  public static func closeOverLimit(a: Double, b: Double, delta: Double? = 0) -> Bool {
+   
+    return a >= (b - delta!);
+    
+  }
+  
+  //Rounds to specific number of decimal places
+  public static func trimDecimal(a: Double, places: Int? = 1) -> Double {
+    
+    if places! <= 0 {
+        return a - (a % 1);
+    }
+    
+    var x = Double(10 << (places! - 1));
+    return (round(x * a) / x);
+    
+  }
+  
+  //Snapping to a grid position, one dimension
+  // - parameter pos : current position
+  // - parameter gridUnitSize : multiple to snap to
+  // - parameter offset : optional starting offset
+  public static func snapToClosestGrid(pos: Double, gridUnitSize: Double, offset: Double? = 0) -> Double {
+    
+    if gridUnitSize == 0 {
+      return pos;
+    }
+    
+    var currentPos = trimDecimal(pos);
+    
+    currentPos -= offset!;
+    currentPos = gridUnitSize * round(currentPos / gridUnitSize);
+    
+    return offset! + currentPos;
+  }
+  
+  //Snapping down to grid position, one dimension
+  // - parameter pos : current position
+  // - parameter gridUnitSize : multiple to snap to
+  // - parameter offset : optional starting offset
+  public static func snapDownToGrid(pos: Double, gridUnitSize: Double, offset: Double? = 0) -> Double {
+   
+   if gridUnitSize == 0 {
+      return pos;
+    }
+    
+    var currentPos = trimDecimal(pos);
+    
+    currentPos -= offset!;
+    currentPos = gridUnitSize * floor(currentPos / gridUnitSize);
+    
+    return offset! + currentPos;
+  }
+  
+  //Snapping up to grid position, one dimension
+  // - parameter pos : current position
+  // - parameter gridUnitSize : multiple to snap to
+  // - parameter offset : optional starting offset
+  public static func snapUpToGrid(pos: Double, gridUnitSize: Double, offset: Double? = 0) -> Double {
+    
+    if gridUnitSize == 0 {
+      return pos;
+    }
+    
+    var currentPos = trimDecimal(pos);
+    
+    currentPos -= offset!;
+    currentPos = gridUnitSize * ceil(currentPos / gridUnitSize);
+    
+    return offset! + currentPos;
+  }
 
   public static func lerp(start: Double, end: Double, weight: Double) -> Double {
     return (1.0 - weight) * start + weight * end
