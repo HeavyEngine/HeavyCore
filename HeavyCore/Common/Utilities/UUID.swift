74fdba037965770d21d8a4e92de209bac3fc882f
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-05-28 18:49:04 -0400

    UUID documentation

--- HeavyCore/Common/Utilities/UUID.swift
+++ HeavyCore/Common/Utilities/UUID.swift
@@ -8,9 +8,11 @@
 
 import Foundation
 
+///  Used for unique IDs. Uses CFUUID as it's base.
 public struct UUID {
   public let cfuuid = CFUUIDCreate(nil)
 
+  /// Return the ```String``` value of this UUID.
   public var string: String {
     return CFUUIDCreateString(nil, cfuuid) as String
   }
