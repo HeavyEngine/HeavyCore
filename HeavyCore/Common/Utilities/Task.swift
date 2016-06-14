809a05dc192dc05a391ea87b4aca5976b7b275a1
Dylan Wreggelsworth <dylan@bvrgroup.us>
2016-06-14 01:33:14 -0400

    Refined task API. --------------------------------------
    * Improved Task playground examples
    * Improved Task API
    * Began Time testing class

--- HeavyCore/Common/Utilities/Task.swift
+++ HeavyCore/Common/Utilities/Task.swift
@@ -12,22 +12,49 @@ public class Task {
   public typealias Closure = (current: Time) -> ()
   private var cancelled = false
   private var interval: Time
+  private var indefinite: Bool
   private var count: Int
   private var closure: Closure
-  public init(interval: Time, count: Int, closure: Closure) {
+
+  ///  Creates a `Task` that will run a number of times.
+  ///
+  ///  - parameter every: How long between the times this task should be run.
+  ///  - parameter repeatCount: How many times this task should be repeat.
+  ///  - parameter closure:  A closure that takes in the current time that we can use for time based side effects.
+  public init(every interval: Time, repeatCount count: Int, closure: Closure) {
     self.interval = interval
     self.count = count
+    self.indefinite = false
     self.closure = closure
   }
-  public init(interval: Time, closure: Closure) {
+
+  ///  Creates a `Task` that will indefinitely until it is cancelled.
+  ///
+  ///  - parameter every: How long between the times this task should be run.
+  ///  - parameter closure:  A closure that takes in the current time that we can use for time based side effects.
+  public init(every interval: Time, closure: Closure) {
     self.closure = closure
     self.interval = interval
     self.count = 0
+    self.indefinite = true
   }
+
+  ///  Creates a `Task` that will run once after a set time.
+  ///
+  ///  - parameter delay: How long before this task is run.
+  ///  - parameter closure:  A closure that takes in the current time that we can use for time based side effects.
+  public init(after delay: Time, closure: Closure) {
+    self.closure = closure
+    self.interval = delay
+    self.count = 1
+    self.indefinite = false
+  }
+
+  ///  Start the task.
   public func start() {
     let timer = Timer()
     var currentTime = timer.now
-    var accumulator = Time(0)
+    var accumulator = Time()
     let operationQueue = NSOperationQueue()
     operationQueue.addOperationWithBlock {
       repeat {
@@ -41,10 +68,15 @@ public class Task {
         while(accumulator >= self.interval) {
           self.closure(current: currentTime)
           self.count = self.count >= 1 ? self.count - 1 : 0
-          self.cancelled = self.count == 0 || self.cancelled == true ? true : false
+          self.cancelled = (self.count == 0 && self.indefinite == false)  || self.cancelled == true ? true : false
           accumulator = accumulator - self.interval
         }
       } while !self.cancelled
     }
+  }
+
+  ///  Stop the task.
+  public func stop() {
+    self.cancelled = true
   }
 }
--- Playgrounds/Core.playground/Pages/Timer.xcplaygroundpage/Contents.swift
+++ Playgrounds/Core.playground/Pages/Timer.xcplaygroundpage/Contents.swift
@@ -27,20 +27,35 @@ for frame in 0.stride(to: 5000, by: 1) {
 let loopEnd = fpsTimer.now
 loopEnd - loopStart
 
-var startT: Time?
-var lastT: Time?
-var priorT: Time?
+var startT = Time()
+var priorT = Time()
+var lastT  = Time()
+let task1: Task
+task1 = Task(every: 0.5.seconds) { time in
+  if startT == 0.seconds {
+    startT  = time
+    lastT   = time
+    priorT  = time
+  } else {
+    lastT = time
+    if lastT - startT >= 3.seconds {
+      print("Task1: stopping")
+      task1.stop()
+      return
+    } else {
+      print("Task1: \((lastT - priorT).seconds) - Runs indefinitely, but we'll stop after 2 seconds from inside the closure.")
+    }
+    priorT = lastT
+  }
+}
+task1.start()
 
-Task(interval: 1.seconds, count: 5) { time in
-  if startT == nil {
-    startT = time
-  }
-  priorT = lastT
-  lastT = time
-  if let start = startT, last = lastT, prior = priorT {
-    "currentDuration \((last - start).milliseconds)"
-    "\n delta \((last - prior).milliseconds)"
-  }
+Task(after: 1.seconds) { _ in
+  print("Task2: One second delayed task.")
+}.start()
+
+Task(every: 1.seconds, repeatCount: 3) { _ in
+  print("Task3: Runs after 1 second, three times.")
 }.start()
 
 //: [Next](@next)
